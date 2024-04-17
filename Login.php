3e9645d2d06d3e933a678c4a0ae26f38c8521c78
Basyan_ <reisdaniel162@gmail.com>
2024-04-16 23:36:51 -0300

    Update Login.php

---
+++ Login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+<?php
+if(isset($_REQUEST['valor']) and ($_REQUEST['valor'] == 'enviado'))
+{
+    $Botao = $_POST ["Botao"];
+    
+    if ($Botao == "Login")
+    {
+        session_start();
+        $_SESSION["control"] = "logado";
+        include "Logado.php";
+    }
+    if ($Botao == "Cadastro")
+    {
+        session_start();
+        $_SESSION["control"] = "new";
+        echo "<a href=\"Cadastro.php\">Novo</a>"
+    }
+}
+else
+{
+    ?>
+    <form action="Login.php?valor=enviado" method="post">
+    Usuário: <br>
+    <input type="text" placeholder="Email" name="usuario"><br>
+    Senha: <br>
+    <input type="password" name="senha" placeholder="Senha" maxlenght="8"><br>
+
+    <input type="submit" value="Logar">
+    <input type="submit" value="Cadastro">
+    <input type="reset" value="Limpar">
+
+    </form>
+    <?php
+}
+?>
+
+
+
+
+
+
+
+</body>
+</html>
+    
+    
