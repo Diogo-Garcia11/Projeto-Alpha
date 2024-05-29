5cd7f360cc0cdd5d597bad76fffcc1cec932f3a3
Murillo Prevelato <prevelatomurillo@gmail.com>
2024-05-29 14:03:05 -0300

    Update Vitrine.php

--- Vitrine.php
+++ Vitrine.php
@@ -6,6 +6,14 @@
     <title>Vitrine</title>
 </head>
 <body>
+<?php
+
+?>
+    <form action="Vitrine.php?valor=enviado" method="post">
+
+    <input type="submit" value="Produto 1" name="Prod 1">
+    <input type="submit" value="Produto 2" name="Prod 2">
+    </form>
     
 </body>
 </html>
