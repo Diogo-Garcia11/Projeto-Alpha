bed31d914f6fc3941420b702dd2de40f1dc9e72a
Diogo Garcia <diogo.garcia3@etec.sp.gov.br>
2024-06-06 15:29:10 -0300

    produto consertado eu acho

--- Produto.php
+++ Produto.php
@@ -8,48 +8,59 @@
 <body>
 <?php
 session_start();
-    if(isset($_REQUEST['valor']) and ($_REQUEST['valor'] == 'enviado'))
+if(1==1)
     {
-        if(isset($_POST['id_produto'])) 
+        if(isset($_SESSION["idProd"]))
         {
-
             include "conexao.php";
-        
-            $id_produto = $_POST['idProd'];
+           
+            $id_produto = $_SESSION['idProd'];
         
             $query = $conexao->prepare("SELECT nome_prod, valor_prod, des_prod FROM tb_produto WHERE id_produto = ?");
             $query->bindParam(1, $id_produto);
             $query->execute();
-        
+            $Linha = $query -> fetch(PDO:: FETCH_OBJ);
+
+            
+            $nomeProd = $Linha -> nome_prod;
+            $valorProd = $Linha -> valor_prod;
+            $desProd = $Linha -> des_prod;
+
             if($query->rowCount() > 0) {
                 $produto = $query->fetch(PDO::FETCH_OBJ);
 
                 $_SESSION['produto_selecionado'] = $produto;
                 
-                echo "<h1>{$produto['nome_prod']}</h1>";
-                echo "<p>Valor: R$ {$produto['valor_prod']}</p>";
-                echo "<p>Descrição: {$produto['des_prod']}</p>";
-                echo "<form action='Login.php' method='post'>";
-                echo "<input type='submit' name='Enviar' value='Comprar'>";
-                echo "</form>";
-            } else {
+                echo "<p>Nome do Produto:$nomeProd</p>";
+                echo "<p>Valor: R$ $valorProd</p>";
+                echo "<p>Descrição: $desProd</p>";
+                ?>
+                <form action='Produto.php?valor=enviado' method='post'>
+                <input type='submit' name='BotaoEnviar' value='Comprar'>
+                </form>
+                <?php
+                
+            } 
+            else 
+            {
                 echo "<p>Produto não encontrado.</p>";
             }
-        } 
-        else 
+        }
+        else
         {
-            echo "<p>Nenhum produto selecionado.</p>";
+            echo"<p>produto nao encontrado</p>";
+        }
+        if(isset($_REQUEST['valor']) and ($_REQUEST['valor'] == 'enviado'))
+        {
+            header('location:Login.php'); 
         }
 
         
-    }
+}
 
 
-    else{
-    ?>
-    <form action="Produto.php?valor=enviado" method="post">
-    <input type="submit" name="Enviar" value="Comprar">
-    </form>
+else{
+?>
 </body>
 </html>
 <?php
--- Vitrine.php
+++ Vitrine.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 <?php
+session_start();
 if(1==1)
 {
 
